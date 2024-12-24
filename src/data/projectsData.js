a6684918fd9729ed9c24e807b564eb704c91bcf5
acharnovich <charnovich@gmail.com>
2024-12-24 03:41:09 -0500

    adding server-tracker project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,9 +2,9 @@
 const projectsData = [
     {
       id: 1,
-      title: 'Python/Django Inventory System',
-      description: 'Manage servers or endpoint devices for refreshes. Notifies IT department when systems are due for upgrades. ',
-      github: 'https://github.com/username/devops-platform',
+      title: 'React and Python Server Information Application',
+      description: 'View a collection of servers at a glance. Quickly view information a server such as packages installed or assoiciated team.',
+      github: 'https://github.com/acharnovich/server_tracker',
       demo: 'https://devops-platform-demo.com',
       image: 'https://via.placeholder.com/400x300', // Replace with real project image
     },
@@ -20,7 +20,7 @@ const projectsData = [
       id: 3,
       title: 'Portfolio Website',
       description: 'Personal Portfolio React Website',
-      github: 'https://github.com/username/portfolio',
+      github: 'https://github.com/acharnovich/my-portfolio',
       demo: 'https://portfolio-demo.com',
       image: 'https://via.placeholder.com/400x300',
     },
